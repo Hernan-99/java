30eac82c656620adcd5912fe4df474ce8bd99de7
Hernan <hernan24744@gmail.com>
2024-02-17 20:57:04 -0300

    primer ejercicio tp

---
+++ ejercicios/src/main/java/org/example/EjercicioUno.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+        //Crear un programa en Java que lea dos números enteros por teclado y los
+        // muestre por pantalla.
+
+        Scanner entrada = new Scanner(System.in);
+        int n1, n2;
+        System.out.print("Ingrese el primer numero: ");
+        n1 = entrada.nextInt();
+
+        System.out.print("Ingrese el segundo numero: ");
+        n2 = entrada.nextInt();
+
+        System.out.print("El primer numero ingresado: " + n1 + ". El segundo numero ingresado: " + n2 + ".");
+
+    }
+}
