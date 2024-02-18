17038fd219fba145a7ea306755f41106db99838a
Hernan <hernan24744@gmail.com>
2024-02-17 21:01:35 -0300

    Segundo ejercicio tp

---
+++ ejercicios/src/main/java/org/example/EjercicioDos.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EjercicioDos {
+    public static void main(String[] args) {
+        //Crear un programa en Java que lea un nombre y muestre por pantalla:
+        // “Buenos dias nombre_introducido”
+
+        Scanner entrada = new Scanner(System.in); // inicializo el escaner para entrada de datos
+        String nombre; // declaro la variable donde voy guardo el nombre
+        System.out.print("Ingrese su nombre: "); // pido nombre al usuario
+        nombre = entrada.nextLine(); // iguale el nombre al dato proporcionado por teclado
+
+        System.out.println("Buenos dias " + nombre); // imprimo
+    }
+}
